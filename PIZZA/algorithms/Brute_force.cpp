9a3c67f18f747bdbcd16f3be1e6c5b26e682134d
guptanitin280 <106119089@nitt.edu>
2023-02-10 22:06:13 +0530

    Initialising FYP directory

---
+++ PIZZA/algorithms/Brute_force.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Asus-2021 on 2/10/2023.
+//
+
+#include "Brute_force.h"
---
+++ PIZZA/algorithms/Score.h
@@ -0,0 +1,14 @@
+//
+// Created by Asus-2021 on 2/10/2023.
+//
+
+#ifndef FYP_SCORE_H
+#define FYP_SCORE_H
+
+
+class Score {
+
+};
+
+
+#endif //FYP_SCORE_H
