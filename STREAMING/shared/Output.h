111f3859693e4fcad7fadb4995a967181df00455
Ritik Mittal <mittalritik365@gmail.com>
2023-04-12 11:24:29 +0530

    output file added

---
+++ STREAMING/shared/Output.h
@@ -0,0 +1,12 @@
+#ifndef FYP_OUTPUT_H
+#define FYP_OUTPUT_H
+#include <bits/stdc++.h>
+using namespace std;
+
+class Output {
+public:
+  int numServers;
+  map<int, vector<int>> videosServed;
+};
+
+#endif // FYP_OUTPUT_H
