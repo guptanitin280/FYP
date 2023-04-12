9cfbd4cb9b7d7f3ac4034b37cc307f50f157df02
rishab <rishabnahar2025@gmail.com>
2023-04-12 11:29:22 +0530

    base

--- STREAMING/shared/Score.cpp
+++ STREAMING/shared/Score.cpp
@@ -2,3 +2,28 @@
 // Created by risha on 12-04-2023.
 //
 
+#include "Score.h"
+
+int Score::calculate(const Input &input, const Output &output) {
+    int val = 0;
+    for (int i = 0; i < input.numberOfClients; i++) {
+        bool acceptable = true;
+        for (string s : input.featureLiked[i]) {
+            if (!output.features.count(s)) {
+                acceptable = false;
+                break;
+            }
+        }
+        if (!acceptable)
+            continue;
+        for (string s : input.featureDisLiked[i]) {
+            if (output.features.count(s)) {
+                acceptable = false;
+                break;
+            }
+        }
+        if (acceptable)
+            val++;
+    }
+    return val;
+}
--- STREAMING/shared/Score.h
+++ STREAMING/shared/Score.h
@@ -5,4 +5,11 @@
 #ifndef STREAMING_SCORE_H
 #define STREAMING_SCORE_H
 
+#include "Input.h"
+#include "Output.h"
+class Score {
+public:
+    static int calculate(const Input &input, const Output &output);
+};
+
 #endif //STREAMING_SCORE_H
